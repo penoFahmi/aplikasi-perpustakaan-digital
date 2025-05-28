94cf4354062a56eed524d1f8d62a3d2f806776e8
penoFahmi <penofahmi@gmail.com>
2025-05-28 19:15:05 +0700

    push

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@ifump.net',
+            'password' => Hash::make('password'),
+            'membership_date' => Carbon::now()
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\StockController;
+use App\Http\Controllers\UserController;
+
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    //Akun
+    // Route::controller(UserController::class)->group(function(){
+    //     Route::get('/user', 'index');
+    //     Route::post('/user/store', 'store');
+    //     Route::patch('/user/{id}/update', 'update');
+    //     Route::get('/user/{id}','show');
+    //     Route::delete('/user/{id}', 'destroy');
+    // });
+
+    Route::apiResource('user', UserController::class);
+    // Route::apiResource('customer', CustomerController::class);
+    // Route::apiResource('barang', BarangController::class);
+    // Route::apiResource('stock', StockController::class);
+    // Route::apiResource('order', OrderController::class);
+
+});
+
+
+
+
+
